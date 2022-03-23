9e2e7884a5831cb6d2d5be37fa1e28b8fce3107c
Abdul Barahow <barahow1994@gmail.com>
2022-03-23 11:26:22 +0100

    Intialize game  and create the snake

--- snake.js
+++ snake.js
@@ -49,19 +49,45 @@ function update(event) {
   if (event.keyCode == 40 && direction != 'up') direction = 'down';
 }
 
-function createGrid() {
-  context.fillStyle = "blue";
-  context.fillRect(0, 0, 16 * GRID_SIZE, 16 * GRID_SIZE); //filling our grid 
+// cheack collision function
+function collision(head, array) {
+  for (let i = 0; i < array.length; i++) {
+    if (head.x == array[i].x && head.y == array[i].y) {
+      return true;
+    }
+  }
+  return false;
 }
 
-function createSnake() {
-  for (i = 0; i < snake.length; i++) {
-    context.fillStyle = "yellow";
-    context.fillRect(snake[i].x, snake[i].y, GRID_SIZE, GRID_SIZE);
+function initializeGame() {
+
+  if (snake[0].x > 15 * GRID_SIZE && direction == "right")
+    snake[0].x = 0;
+  if (snake[0].x < 0 && direction == 'left')
+    snake[0].x = 16 * GRID_SIZE;
+  if (snake[0].y > 15 * GRID_SIZE && direction == "down")
+    snake[0].y = 0;
+  if (snake[0].y < 0 && direction == 'up')
+    snake[0].y = 16 * GRID_SIZE;
+
+  for (i = 1; i < snake.length; i++) {
+    if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+
+
+      clearInterval(game);
+      alert('Game Over :(');
+    }
+
+
+
   }
 }
 
-function drawFood() {
-  context.fillStyle = "red";
-  context.fillRect(food.x, food.y, GRID_SIZE, GRID_SIZE);
-}
+
+
+  createGrid()
+
+  createSnake();
+  drawFood();
+
+
