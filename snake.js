b6726d2b64ebe8a04245f84dfada53dfea245da9
Abdul Barahow <barahow1994@gmail.com>
2022-03-23 11:20:55 +0100

    Drawing the grid, snake, and food functions

--- snake.js
+++ snake.js
@@ -13,8 +13,8 @@ snake[0] = {
 }
 let direction = "right";
 let food = {
-  x: Math.floor(Math.random() * 17 + 1) * GRID_SIZE,
-  y: Math.floor(Math.random() * 15 + 3) * GRID_SIZE
+  x: Math.floor(Math.random() * 15 + 1) * GRID_SIZE,
+  y: Math.floor(Math.random() * 15 + 1) * GRID_SIZE
 
 
 
@@ -47,4 +47,21 @@ function update(event) {
   if (event.keyCode == 38 && direction != 'down') direction = 'up';
   if (event.keyCode == 39 && direction != 'left') direction = 'right';
   if (event.keyCode == 40 && direction != 'up') direction = 'down';
+}
+
+function createGrid() {
+  context.fillStyle = "blue";
+  context.fillRect(0, 0, 16 * GRID_SIZE, 16 * GRID_SIZE); //filling our grid 
+}
+
+function createSnake() {
+  for (i = 0; i < snake.length; i++) {
+    context.fillStyle = "yellow";
+    context.fillRect(snake[i].x, snake[i].y, GRID_SIZE, GRID_SIZE);
+  }
+}
+
+function drawFood() {
+  context.fillStyle = "red";
+  context.fillRect(food.x, food.y, GRID_SIZE, GRID_SIZE);
 }
